6db6d755d72bf764a8ad7e9a9f78fc3a141af591
GuoTingRui <azsxdc890902@gmail.com>
2022-01-03 22:44:17 +0800

    建立資料表及欄位

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,14 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->integer('sex');
+            $table->date('birthday');
+            $table->string('address');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->integer('auth');
             $table->timestamps();
         });
     }
